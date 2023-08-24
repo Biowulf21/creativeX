102de0cfc07efd9e298259a3e521132457c51722
James Jilhaney <notail2111@gmail.com>
2023-08-24 20:48:30 +0800

    Added routes for usercontroller

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+/* Route::middleware('auth:sanctum')->get('/user', function (Request $request) { */
+/*     return $request->user(); */
+/* }); */
+
+
+Route::group(['prefix' => 'api'], function () {
+    Route::controller(UserController::class)->group(function(){
+        Route::post('/login', 'loginUser');
+        Route::post('/signup', 'store');
+        Route::get('/logout', 'logOutUser');
+    });
+
+    // TODO: add routes for other controlers
 });
