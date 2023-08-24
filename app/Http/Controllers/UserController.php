fa584eda686634a7176303956926db758b17e106
James Jilhaney <notail2111@gmail.com>
2023-08-24 20:27:52 +0800

    added the rest of the UserController class

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,15 +6,40 @@ use App\Http\Repositories\UserRepository\UserRepositoryInterface;
 use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+
+class UserController extends Controller
+{
     private $user_repository;
     public function __construct(UserRepositoryInterface $user_repository){
         $this->user_repository = $user_repository;
     }
 
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Sign up a new user.
+     */
+
     public function logInUser(LoginRequest $request): Response
     {
         return $this->user_repository->login($request->all());
     }
+
     public function logOutUser(): Response
     {
         return $this->user_repository->signOut();
@@ -26,3 +51,35 @@ use Illuminate\Http\Response;
         return $this->user_repository->signUp($request->all());
     }
 
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
