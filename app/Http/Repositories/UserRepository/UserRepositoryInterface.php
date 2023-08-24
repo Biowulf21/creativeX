fabd2b510620b7df9a158f32c503731c31cda441
James Jilhaney <notail2111@gmail.com>
2023-08-24 19:46:29 +0800

    Added return type comment in interface

--- app/Http/Repositories/UserRepository/UserRepositoryInterface.php
+++ app/Http/Repositories/UserRepository/UserRepositoryInterface.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Repositories\UserRepository;
 
+use Illuminate\Http\Response;
+
 interface UserRepositoryInterface
 {
-    public function login();
-    public function signUp();
+    /**
+     * @return Response
+     * @param array<int,mixed> $data
+     */
+
+    public function login(array $data);
+
+    /**
+     * @return Response
+     * @param array<int,mixed> $data
+     */
+
+    public function signUp(array $data);
+
+    /**
+     * @return void
+     */
     public function singOut();
 }
