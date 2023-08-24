45f15667634fbcb614504aade1faa4d8cd44b2c2
James Jilhaney <notail2111@gmail.com>
2023-08-24 20:26:47 +0800

    dependency injection for user repository

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,3 +6,8 @@ use App\Http\Repositories\UserRepository\UserRepositoryInterface;
 use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+    private $user_repository;
+    public function __construct(UserRepositoryInterface $user_repository){
+        $this->user_repository = $user_repository;
+    }
+
