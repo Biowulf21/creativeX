dda291202b3aa124cdd92e0e16ef626d4e948e74
James Jilhaney <notail2111@gmail.com>
2023-08-24 14:18:11 +0800

    created user repository

---
+++ app/Http/Repositories/UserRepository/UserRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Repositories\UserRepository;
+
+class UserRepository implements UserRepositoryInterface
+{
+
+    public void function login()
+    {
+
+    }
+    public function signUp()
+    {
+
+    }
+    public function singOut()
+    {
+
+    }
+}
