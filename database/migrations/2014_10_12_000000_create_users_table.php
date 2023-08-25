02562abcb22745c7ecfbe675933bf7f4b1e3d8a6
James Jilhaney <notail2111@gmail.com>
2023-08-25 18:43:35 +0800

    Added account handle and bio to migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('account_handle', 15);
-            $table->string('bio', 160);
+            $table->string('account_handle', 15)->unique('account_handle');
+            $table->string('bio', 160)->nullable(null);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
