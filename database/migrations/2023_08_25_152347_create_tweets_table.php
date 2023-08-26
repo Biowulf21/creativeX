35511052f05c2bf71b0cceba38277c12d06af806
James Jilhaney <notail2111@gmail.com>
2023-08-26 10:27:05 +0800

    Added migration for tweet model

--- database/migrations/2023_08_25_152347_create_tweets_table.php
+++ database/migrations/2023_08_25_152347_create_tweets_table.php
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('tweets', function (Blueprint $table) {
             $table->id();
+            $table->string('tweet_body', 280);
+
+            $table->unsignedBigInteger('replying_to')->nullable();
+            $table->foreign('replying_to')->references('id')->on('tweets');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->unsignedBigInteger('likes_count')->default(0);
+            $table->unsignedBigInteger('retweets_count')->default(0);
+            $table->boolean('is_retweet')->default(false);
+
             $table->timestamps();
         });
     }
