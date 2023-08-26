e9fc746e26b5c8ef8c0ea3f2da1aba06f0b56351
James Jilhaney <notail2111@gmail.com>
2023-08-26 11:13:46 +0800

    Created tweet repository to implement the tweet interface

---
+++ app/Http/Repositories/TweetRepository/TweetRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Repositories\TweetRepository;
+
+use App\Http\Repositories\TweetRepository\TweetRepositoryInterface;
+
+class TweetRepository implements TweetRepositoryInterface
+{
+    public function createTweet(array $data)
+    {
+    }
+
+    public function getTweet(int $id)
+    {
+    }
+
+    public function getTweets()
+    {
+    }
+
+    public function deleteTweet(int $id)
+    {
+    }
+
+}
