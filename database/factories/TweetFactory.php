2364d89e3e94f1dbd786822b64ba00439d2f052c
James Jilhaney <notail2111@gmail.com>
2023-08-26 21:48:59 +0800

    assigned long functions to variables for clarity

--- database/factories/TweetFactory.php
+++ database/factories/TweetFactory.php
@@ -25,13 +25,17 @@ class TweetFactory extends Factory
         //It uses the fake() helper function to generate a random element from an array of values.
         //The array contains two values: a function that returns a random tweet ID,
         //and null. This is to ensure that the tweet can be a reply to another tweet,
+        $tweetIDorNull = fake()->randomElement([function () {
+            $tweet = Tweet::inRandomOrder()->first();
+            return $tweet ? $tweet->id : null;
+        }, null]),
 
-        'replying_to' => fake()->randomElement([function(){
-                $tweet = Tweet::inRandomOrder()->first->value('id');
-                return $tweet ??= null;
-                }, null]),
+        $userID = function () {
+            return User::inRandomOrder()->first()->id;
+        },
 
-        'user_id' => function () { return User::factory()->make()->id; },
+        'replying_to' => $tweetIDorNull,
+        'user_id' => $userID,
         'likes_count' => fake()->numberBetween(0, 100),
         'retweets_count' => fake()->numberBetween(0, 100),
         'is_retweet' => fake()->boolean,
