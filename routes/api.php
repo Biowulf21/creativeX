e3e1110ff7bc9b0d9843debd39d8aa2d686c4085
James Jilhaney <notail2111@gmail.com>
2023-08-26 10:59:00 +0800

    Small changes

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,6 @@
 use App\Http\Controllers\RetweetController;
 use App\Http\Controllers\TweetController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
