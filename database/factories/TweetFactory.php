7526af7ec5352ad286fe2fdfc799095d3c2d4788
James Jilhaney <notail2111@gmail.com>
2023-08-26 21:26:20 +0800

    Added factory for Tweet

--- database/factories/TweetFactory.php
+++ database/factories/TweetFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Tweet;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +19,13 @@ class TweetFactory extends Factory
     public function definition(): array
     {
         return [
-            //
-        ];
+        'tweet_body' => fake()->realText(280),
+        'replying_to' => fake()->randomElement([Tweet::inRandomOrder()->value('id'), null]),
+        'user_id' => function () { return User::factory()->make()->id; },
+        'likes_count' => fake()->numberBetween(0, 100),
+        'retweets_count' => fake()->numberBetween(0, 100),
+        'is_retweet' => fake()->boolean,
+        'tweet_attachment_link' => fake()->imageUrl(), // Generate a sample image URL
+                ];
     }
 }
