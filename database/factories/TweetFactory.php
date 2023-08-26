90fc7dc41c216d39e1af72b9f778aeaa6e00cfe5
James Jilhaney <notail2111@gmail.com>
2023-08-26 21:58:20 +0800

    I'm an idiot, refactored again

--- database/factories/TweetFactory.php
+++ database/factories/TweetFactory.php
@@ -24,7 +24,7 @@ class TweetFactory extends Factory
             return $tweet ? $tweet->id : null;
         }, null]);
 
-        $userID = fn () => User::inRandomOrder()->first()->id;
+        $userID = User::inRandomOrder()->first()->id;
 
         return [
         'tweet_body' => fake()->realText(280),
