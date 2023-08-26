b5fdd962fc9e6ccbf009949b16aae2b853f869ab
James Jilhaney <notail2111@gmail.com>
2023-08-26 10:26:57 +0800

    Added attributes for tweet model

--- app/Models/Tweet.php
+++ app/Models/Tweet.php
@@ -4,8 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Tweet extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'tweet_body',
+        'likes_count',
+        'retweet_count',
+        'is_retweet',
+    ];
+
+    protected $guarded = [
+        'replying_to',
+        'user_id',
+        'likes_count',
+        'retweets_count',
+        'is_retweet'
+    ];
+
+
+
 }
