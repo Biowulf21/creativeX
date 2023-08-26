2c70eb10a318f75d1cc962aba4873a90803e5d56
James Jilhaney <notail2111@gmail.com>
2023-08-26 19:18:30 +0800

    Check if tweet_attachment payload is not null and upload if it's not

--- app/Http/Repositories/TweetRepository/TweetRepository.php
+++ app/Http/Repositories/TweetRepository/TweetRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Repositories\TweetRepository;
 
+use App\Http\Repositories\Attachment\AttachmentRepositoryInterface;
 use App\Http\Repositories\TweetRepository\TweetRepositoryInterface;
 use App\Models\Tweet;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
@@ -11,11 +12,13 @@ class TweetRepository implements TweetRepositoryInterface
 {
     public function createTweet(array $data)
     {
+
         $user_id = Auth::user()->id;
 
         $retweet_count = 0;
         $likes_count = 0;
         $is_retweet = $data['is_retweet'] == '1' ? true : false;
+        $tweet_attachment = $data['tweet_attachment'] ??= null;
 
         $data['user_id'] = $user_id; // Set the user_id
 
@@ -28,11 +31,21 @@ class TweetRepository implements TweetRepositoryInterface
         $new_tweet = new Tweet;
         $new_tweet->fill($data);
         $new_tweet->user_id = $user_id;
+        $new_tweet->tweet_attachment_link = $tweet_attachment;
+
+        if ($tweet_attachment != null){
+            $attachmentRepository = app(AttachmentRepositoryInterface::class);
+            $upload_response = $attachmentRepository->uploadAttachment($tweet_attachment);
+            if($upload_response->status() == 500) return $upload_response;
+        }
+
+        echo $new_tweet->tweet_attachment;
         $new_tweet->save();
 
         return response()->json(['message' => 'Tweet created successfully', 'tweet_body'=>$new_tweet], 200);
 
     }
+
 
     public function updateTweet(int $id, array $data)
     {
