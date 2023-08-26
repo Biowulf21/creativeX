e7a3e975854f0b61c2d63ae12c61b95c2729cca5
James Jilhaney <notail2111@gmail.com>
2023-08-26 11:16:08 +0800

    binded tweet repository and its interface

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -2,9 +2,11 @@
 
 namespace App\Providers;
 
+use App\Http\Repositories\TweetRepository\TweetRepositoryInterface;
 use App\Http\Repositories\UserRepository\UserRepository;
 use App\Http\Repositories\UserRepository\UserRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
+use TweetRepository;
 
 class RepositoryServiceProvider extends ServiceProvider
 {
@@ -22,5 +24,7 @@ class RepositoryServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
+        $this->app->bind(TweetRepositoryInterface::class, TweetRepository::class);
+
     }
 }
