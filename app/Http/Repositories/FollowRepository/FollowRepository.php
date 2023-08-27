3fe1549a8cde860c0ab39e9494782dac8232c159
James Jilhaney <notail2111@gmail.com>
2023-08-27 16:43:45 +0800

    implemented function to create new follow record

--- app/Http/Repositories/FollowRepository/FollowRepository.php
+++ app/Http/Repositories/FollowRepository/FollowRepository.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Repositories\FollowRepository;
 
+use App\Exceptions\AlreadyFollowingException;
 use App\Models\Follow;
 
 
 class FollowRepository implements FollowRepositoryInterface
 {
 
+    //NOTE: made public in case client needs to know if a user is following another user
     public function isFollowing(int $followingUserId, int $followerUserId)
     {
         $isFollowing = Follow::where('follower_id', $followerUserId)
@@ -16,4 +18,47 @@ class FollowRepository implements FollowRepositoryInterface
         return $isFollowing;
     }
 
+
+    public function follow(int $followingUserId, int $followerUserId)
+    {
+        try {
+
+            $isFollowing = $this->isFollowing($followingUserId, $followerUserId);
+            if ($isFollowing) throw AlreadyFollowingException;
+
+            $wasPreviouslyFollowing = Follow::withTrashed(true)
+                ->where('follower_user_id', $followerUserId)
+                ->where('following_user_id', $followingUserId)
+                ->whereNotNull('deleted_at')
+                ->first();
+
+            if ($wasPreviouslyFollowing) {
+                $wasPreviouslyFollowing->restore();
+            }
+
+            $follow = Follow::create([
+                'follower_user_id' => $followerUserId,
+                'following_user_id' => $followingUserId]);
+
+
+        } catch (\Throwable $th) {
+            return response()->json(['error' => $th->getMessage()], 500);
+        }
+        return response()
+            ->json(['message' => 'followed successfully',
+                    'follower'=> $follow->follower(),
+                    'now_following' =>$follow->following()],200);
+    }
+
+    public function unfollow(int $followingUserId, int $followerUserId)
+    {
+    }
+
+    public function getAllFollowers(int $userId)
+    {
+    }
+
+    public function getAllFollowing(int $userId)
+    {
+    }
 }
