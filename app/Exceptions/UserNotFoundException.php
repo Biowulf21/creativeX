d15bf05d5170a7fe9efff4688de42bbfee3a764c
James Jilhaney <notail2111@gmail.com>
2023-08-28 00:05:52 +0800

    Fixed formatting of exception message

--- app/Exceptions/UserNotFoundException.php
+++ app/Exceptions/UserNotFoundException.php
@@ -8,7 +8,6 @@ class UserNotFoundException extends Exception
 {
     public function render()
     {
-        return response()->json(['message'=>'The user cannot perform this action at this
-            action as they cannot be found'], 404);
+        return response()->json(['message'=>'The user cannot perform this action at this action as they cannot be found'], 404);
     }
 }
