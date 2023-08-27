a72a2082f41a2a2e2267d41e2fb4b7be76c5dde8
James Jilhaney <notail2111@gmail.com>
2023-08-27 19:07:43 +0800

    changed parameter order for better readability and adjusted parameters to routes

--- app/Http/Controllers/FollowController.php
+++ app/Http/Controllers/FollowController.php
@@ -16,35 +16,36 @@ class FollowController extends Controller
 
     public function isFollowing(FollowRequest $request)
     {
-        $follower_id = $request->follower_id;
-        $following_id = $request->following_id;
+        $follower_user_id = $request->follower_id;
+        $following_user_id = $request->following_id;
 
-        return $this->follow_repository->isFollowing($following_id, $follower_id);
+        return $this->follow_repository->isFollowing($following_user_id, $follower_user_id);
 
     }
 
-
-    public function followUser(FollowRequest $request)
+    public function followUser(int $follower_user_id, int $following_user_id)
     {
-        $follower_id = $request->follower_id;
-        $following_id = $request->following_id;
 
         // TODO: implement checking if the user being followed allows
         // their account to be followed
-        return $this->follow_repository->follow($following_id, $follower_id);
+        return $this->follow_repository->follow($follower_user_id, $following_user_id);
     }
 
-    // FollowRequest has the same parameters needed as an UnfollowRequest
-    // Decided not to make another one because of that
-    public function unfollowUser(FollowRequest $request)
+    public function unfollowUser(int $follower_user_id, int $following_user_id)
     {
-        $follower_id = $request->follower_id;
-        $following_id = $request->following_id;
 
-        return $this->follow_repository->unfollow($following_id, $follower_id);
-
+        return $this->follow_repository->unfollow($follower_user_id, $following_user_id);
     }
 
+    public function getFollowers(int $user_id)
+    {
+        return $this->follow_repository->getAllFollowers($user_id);
+    }
 
+    // $following_user_id represents the user who follows other people
+    public function getFollowings(int $user_id)
+    {
+        return $this->follow_repository->getAllFollowing($user_id);
 
+    }
 }
