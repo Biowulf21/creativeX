d4786f4d5745d4ef7b70c11238d4f098281a3617
James Jilhaney <notail2111@gmail.com>
2023-08-27 17:12:05 +0800

    Added general exception for when a nonexistent user tries to perform an action

---
+++ app/Exceptions/UserNotFoundException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+class UserNotFoundException extends Exception
+{
+    public function render()
+    {
+        return response()->json(['message'=>'The user cannot perform this action at this
+            action as they cannot be found'], 404);
+    }
+}
