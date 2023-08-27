2ce7c78adff974174c853942ed7e36e73906a9c1
James Jilhaney <notail2111@gmail.com>
2023-08-27 15:54:04 +0800

    Added fillable and relationship functions in follow model

--- app/Models/Follow.php
+++ app/Models/Follow.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Follow extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    private $fillable = [
+        'follower_user_id',
+        'following_user_id',
+    ];
+
+    public function follower(): HasOne
+    {
+        return $this->hasOne(User::class, 'id', 'follower_user_id');
+
+    }
+
+    public function following(): HasOne
+    {
+        return $this->hasOne(User::class, 'id', 'following_user_id');
+    }
 }
