0a46135fd17f6f91b9f0c2992de584fe2c9c31d6
James Jilhaney <notail2111@gmail.com>
2023-08-28 12:07:46 +0800

    Updated usernotfoundexception to be more extensible

--- app/Exceptions/UserNotFoundException.php
+++ app/Exceptions/UserNotFoundException.php
@@ -6,8 +6,18 @@ use Exception;
 
 class UserNotFoundException extends Exception
 {
+    protected $defaultMessage = 'Cannot perform this operation. User not found.';
+
+        public function __construct($message = null, $code = 0,)
+        {
+            if ($message == null)
+            {
+                $message = $this->defaultMessage;
+            }
+            parent::__construct($message, $code);
+        }
     public function render()
     {
-        return response()->json(['message'=>'The user cannot perform this action at this action as they cannot be found'], 404);
+        return response()->json(['message'=>$this->message], 404);
     }
 }
